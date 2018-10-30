d35a68df8429bb5cc1763c6631ef1a7e848d6bbf
Ana Vargas <avargas000@nyc.yearup.org>
2018-10-30 15:45:25 -0400

    javacommit

---
+++ main.js
@@ -0,0 +1,34 @@
+var prices = [33, 26, 99, 120, 12, 45];
+var sum = 0;
+
+// Your code here
+for (var i=0; i < prices.length; i++ ) {
+ sum += prices[i];
+}
+console.log("The sum is: ", sum);
+
+let counter = 0;
+
+
+
+let counter= -1;
+
+let counter= -1;
+
+while (counter<=100){
+  console.log((100+counter)+" crows on the wall. "+(100+counter)+" crows");
+  console.log("1 fell down nd became a wight.");
+  console.log(counter+99 +" crows on the wall")
+  console.log("")
+  counter;
+
+counter--;
+
+
+if(counter==-100){
+  break;
+
+
+
+}
+}
